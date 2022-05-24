fad99a7c3050123aca0e86a6de0f39233f4ed3f5
ibrahimdenizz <dnzibrahim1@gmail.com>
2022-05-24 13:21:34 +0300

    Add server ai to example

--- dev/src/App.vue
+++ dev/src/App.vue
@@ -1,53 +1,101 @@
 <template>
-  <p
-    :style="{
-      'text-align': 'center',
-    }"
-  >
-    Winner: {{ winner }}
-  </p>
-  <p
-    :style="{
-      'text-align': 'center',
-    }"
-  >
-    FEN: {{ game.fen }}
-  </p>
-  <p
-    :style="{
-      'text-align': 'center',
-    }"
-  >
-    Zobrist Hash: {{ game.zobrist.hash }}
-  </p>
-  <div class="home">
-    <chess-board
-      :size="chessBoardSize"
-      :game="game"
-      v-model:fen="fen"
-      @onMovePlayed="onMovePlayed"
-      @onGameOver="onGameOver"
-    />
-    <div>
-      <div class="btn-group">
-        <button class="btn" @click="undoMove()">Undo</button>
-        <button class="btn" @click="redoMove()">Redo</button>
+  <div>
+    <p
+      :style="{
+        'text-align': 'center',
+      }"
+    >
+      Winner: {{ winner }}
+    </p>
+    <p
+      :style="{
+        'text-align': 'center',
+      }"
+    >
+      FEN: {{ game.fen }}
+    </p>
+    <p
+      :style="{
+        'text-align': 'center',
+      }"
+    >
+      Zobrist Hash: {{ game.zobrist.hash }}
+    </p>
+    <div class="home">
+      <chess-board
+        :size="chessBoardSize"
+        :game="game"
+        v-model:fen="fen"
+        @onMovePlayed="onMovePlayed"
+        @onGameOver="onGameOver"
+      />
+      <div>
+        <div class="btn-group">
+          <button class="btn" @click="undoMove()">Undo</button>
+          <button class="btn" @click="redoMove()">Redo</button>
+        </div>
+        <button class="btn" @click="resetGame()">Reset</button>
+        <button
+          :disabled="gameType === 'two-player'"
+          class="btn"
+          @click="changeGameType('two-player')"
+        >
+          Two player
+        </button>
+        <button
+          :disabled="gameType === 'random-ai'"
+          class="btn"
+          @click="changeGameType('random-ai')"
+        >
+          Random AI
+        </button>
+        <button
+          :disabled="gameType === 'normal-ai'"
+          class="btn"
+          @click="changeGameType('normal-ai')"
+        >
+          Normal AI
+        </button>
+        <div class="radio" v-if="gameType === 'normal-ai'">
+          <p>AI execute environment:</p>
+          <input
+            type="radio"
+            id="server"
+            name="normal-ai-type"
+            value="server"
+            v-model="normalAIType"
+          />
+          <label for="server">Server</label>
+          <input
+            type="radio"
+            id="web"
+            name="normal-ai-type"
+            value="web"
+            v-model="normalAIType"
+          />
+          <label for="web">Web</label>
+          <p v-if="normalAIType === 'web'" style="width: 200px">
+            Be careful. Web version of AI have performance issue after 4 depth
+          </p>
+          <p>
+            <label class="label" for="web">Depth</label>
+            <input
+              type="number"
+              id="web"
+              v-model="normalAIDepth"
+              min="1"
+              max="5"
+            />
+          </p>
+        </div>
       </div>
-      <button class="btn" @click="changeGameType('two-player')">
-        Two player
-      </button>
-      <button class="btn" @click="changeGameType('random-ai')">
-        Random AI
-      </button>
-      <button class="btn" @click="changeGameType('normal-ai')">
-        Normal AI
-      </button>
     </div>
   </div>
 </template>
 
 <script>
 import { ChessBoard, ChessAI, ChessGame } from "@ibrahimdeniz/vue-chessboard";
+import axios from "axios";
 import "@ibrahimdeniz/vue-chessboard/dist/style.css";
 
 const ratio = 0.8;
@@ -63,6 +111,8 @@ export default {
       game,
       randomAI: new ChessAI("random"),
       normalAI: new ChessAI("normal", 3),
+      normalAIType: "server",
+      normalAIDepth: 1,
       winner: null,
       gameType: "normal-ai",
       isPlayTwoAI: false,
@@ -88,7 +138,7 @@ export default {
       if (this.gameType !== "two-player") this.makeAiMove(game);
     },
     makeAiMove(game) {
-      return setTimeout(() => {
+      return setTimeout(async () => {
         if (this.gameType === "random-ai") {
           if (!game.gameOver && game.currentPlayer === "black") {
             const aiMove = this.randomAI.selectMove(game.fen);
@@ -96,9 +146,25 @@ export default {
           }
         } else if (this.gameType === "normal-ai") {
           if (!game.gameOver && game.currentPlayer === "black") {
-            console.log(this.normalAI);
-            const aiMove = this.normalAI.selectMove(game.fen, { debug: true });
-            console.log(aiMove);
+            let aiMove;
+            if (this.normalAIType === "web") {
+              aiMove = this.normalAI.selectMove(game.fen, {
+                debug: true,
+                depth: this.normalAIDepth,
+              });
+            } else {
+              const response = await axios.get(
+                `https://9ze1us9zsl.execute-api.eu-central-1.amazonaws.com/chess-ai`,
+                {
+                  params: {
+                    fen: game.fen,
+                    depth: this.normalAIDepth,
+                  },
+                }
+              );
+              aiMove = response.data;
+            }
+
             game.makeMove(aiMove);
           }
         }
@@ -119,6 +185,9 @@ export default {
     redoMove() {
       this.game.redoMove();
       console.log(this.game.redoHistory);
+    },
+    resetGame() {
+      this.game = new ChessGame();
     },
     async playTwoAI() {
       this.isPlayTwoAI = true;
@@ -153,5 +222,17 @@ export default {
   margin-left: 20px;
   margin-bottom: 20px;
   display: block;
+}
+
+.radio {
+  height: 50px;
+  margin-left: 20px;
+  margin-bottom: 20px;
+  display: block;
+}
+
+.label {
+  display: block;
+  margin-bottom: 10px;
 }
 </style>
